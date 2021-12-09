d8e76f8fcdcbb9123426a682fd628d3b6de88649
NibrasHCDR <95778414+NibrasHCDR@users.noreply.github.com>
2021-12-08 23:03:17 -0300

    Initial commit

--- app/src/main/java/com/example/myapplication/InicioActivity.kt
+++ app/src/main/java/com/example/myapplication/InicioActivity.kt
@@ -2,10 +2,30 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.myapplication.fragments.InicioFragment
+import com.example.myapplication.fragments.NoticeFragment
+import com.example.myapplication.fragments.PerfilFragment
 
 class InicioActivity : AppCompatActivity() {
+
+    private val inicioFragment = InicioFragment()
+    private val noticeFragment = NoticeFragment()
+    private val perfilFragment = PerfilFragment()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_inicio)
+        reemplazarFrament(inicioFragment)
+
+
+    }
+
+    private fun reemplazarFrament(fragment: Fragment){
+        if(fragment !=null){
+            val transaccion = supportFragmentManager.beginTransaction()
+            transaccion.replace(R.id.fragment_container, fragment)
+            transaccion.commit()
+        }
     }
 }
